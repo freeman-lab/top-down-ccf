5591ea7e5ee8baa38cac6189e2b7756d3a149743
freeman <the.freeman.lab@gmail.com>
2016-08-31 17:05:10 -0700

    get area selection working

--- index.js
+++ index.js
@@ -2,31 +2,79 @@ var loadsvg = require('load-svg')
 var css = require('dom-css')
 var position = require('mouse-position')
 var request = require('browser-request')
+var pointinpoly = require('point-in-svg-polygon')
 
-var size = 700
+var scale = 0.5
+var targets = ['VISam_000', 'VISa_000', 'SSp-un_000', 'SSp-m_000', 'SSp-ul_000', 'SSp-ll_000', 'SSp-tr_000', 'MOp_000', 'MOs_000']
  
+var selected = document.createElement('div')
+selected.innerHTML = 'select an area'
+css(selected, {
+  fontFamily: 'monospace',
+  fontSize: '24px',
+  position: 'fixed',
+  left: 10,
+  top: 10,
+  color: 'white'
+})
+document.body.appendChild(selected)
+
 loadsvg('./resources/top_down_outline.svg', function (err, svg) {
   css(svg, {
-    width: size,
-    height: size,
+    width: 1140 * scale,
+    height: 1320 * scale,
     position: 'absolute',
     left: 0,
-    top: 0
+    top: 0,
+    cursor: 'pointer'
   })
   document.body.appendChild(svg)
-  mouse = position(svg)
-  mouse.on('move', function (event) {
-    console.log(event.clientX + ',' + event.clientY)
+
+  document.querySelectorAll('path').forEach(function (d) {
+    css(d, {
+      stroke: 'yellow',
+      strokeWidth: 5
+    })
   })
+
+  var lookup = {}
+  targets.forEach(function (d) {
+    var el = document.querySelector('#' + d)
+    lookup[d] = {
+      el: el,
+      path: el.attributes.d.textContent
+    }
+  })
+
+  mouse = position(svg)  
+  mouse.on('move', function (event) {
+    var pos = [event.clientX * 1 / scale, event.clientY * 1 / scale]
+    for (var t=0; t < targets.length; t++) {
+      var item = lookup[targets[t]]
+      css(item.el, {fill: 'none'})
+    }
+    selected.innerHTML = 'select an area'
+    for (var t=0; t < targets.length; t++) {
+      var item = lookup[targets[t]]
+      var result = pointinpoly.isInside(pos, item.path)
+      if (result) {
+        css(item.el, {fill: 'yellow'})
+        selected.innerHTML = targets[t]
+        break
+      }
+    }
+  })  
 })
 
 var image = document.createElement('img')
-image.src = 'http://connectivity.brain-map.org/api/v2/well_known_file_download/501058982'
+image.src = './resources/background_10.png'
 css(image, {
-  width: size,
-  height: size,
+  width: 1140 * scale,
+  height: 1320 * scale,
   position: 'absolute',
   left: 0,
-  top: 0
+  top: 0,
+  zIndex: -1000
 })
 document.body.appendChild(image)
+
